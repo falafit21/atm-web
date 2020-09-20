689c8ff40c6cdb4fd5d21f425aa46259af79d8f5
6110406147 <phitchayatida.c@ku.th>
2020-09-20 22:42:12 +0700

    first

---
+++ src/main/java/th/ac/ku/atm/controller/CustomerController.java
@@ -0,0 +1,27 @@
+
+
+package th.ac.ku.atm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.ArrayList;
+
+@Controller
+public class CustomerController {
+    ArrayList<Customer> customers = new ArrayList<>();
+
+    @GetMapping("/customer")
+    public String getCustomerPage(Model model) {
+        model.addAttribute("allCustomers", customers);
+        return "customer";  // customer.html
+    }
+    @PostMapping("/customer")
+    public String registerCustomer(@ModelAttribute Customer customer, Model model) {
+        customers.add(customer);
+        model.addAttribute("allCustomers", customers);
+        return "redirect:customer";
+    }
+}
